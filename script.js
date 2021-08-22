2870c36d41553d120059650abeedbdf340c1b835
Edson Lima <edsoneduardo526@gmail.com>
2021-08-22 13:53:42 -0300

    Requisito 7 (take 2)

--- script.js
+++ script.js
@@ -5,7 +5,13 @@ BtnCriarTarefa.addEventListener("click", adicionaTarefa);
 function adicionaTarefa (){
    let tarefa = document.createElement("li");
    tarefa.innerText = textoInput.value;
-   tarefa.className = "itemLista"
+   tarefa.className = "itemLista";
+   tarefa.id = textoInput.value;
+   tarefa.addEventListener("click", trocaBg);
    listaDeTarefas.appendChild(tarefa);
    textoInput.value = "";
 }
+function trocaBg (eventoDeOrigem) {
+    let itemClicado = eventoDeOrigem.target.id;
+    document.getElementById(itemClicado).style.backgroundColor = "rgb(128, 128, 128)";
+} 
