161954a5eb169ead32c86112007fda53f98afe15
Edson Lima <edsoneduardo526@gmail.com>
2021-08-22 14:36:34 -0300

    Requisito 8

--- script.js
+++ script.js
@@ -12,6 +12,14 @@ function adicionaTarefa (){
    textoInput.value = "";
 }
 function trocaBg (eventoDeOrigem) {
+    let itensDaLista = document.getElementsByClassName("itemLista")
+    for (let i = 0; i < itensDaLista.length; i += 1){
+        let identificador = itensDaLista[i].id;
+        let bgColor = document.getElementById(identificador).style.backgroundColor;
+        if (bgColor == "rgb(128, 128, 128)") {
+            document.getElementById(identificador).style.backgroundColor = "white";
+        }
+    }
     let itemClicado = eventoDeOrigem.target.id;
     document.getElementById(itemClicado).style.backgroundColor = "rgb(128, 128, 128)";
-} 
+}
