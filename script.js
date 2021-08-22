590e0c988facbe0926c69b1370f89d8cb1f500aa
Edson Lima <edsoneduardo526@gmail.com>
2021-08-22 13:23:04 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ let BtnCriarTarefa = document.getElementById("criar-tarefa");
 let listaDeTarefas = document.getElementById("lista-tarefas");
 let textoInput = document.getElementById("texto-tarefa");
 BtnCriarTarefa.addEventListener("click", adicionaTarefa);
-function adicionaTarefa (BtnCriarTarefa){
+function adicionaTarefa (){
    let tarefa = document.createElement("li");
    tarefa.innerText = textoInput.value;
+   tarefa.className = "itemLista"
    listaDeTarefas.appendChild(tarefa);
    textoInput.value = "";
 }
